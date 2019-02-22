3d1c9439d3f4cf256c2a1288b7df255ca3c01628
luwei.jin@outlook.com <luwei.jin@outlook.com>
2019-02-22 17:07:15 +0800

    array数组更改

--- New Binary chop/Aotoganerate array/Program.cs
+++ New Binary chop/Aotoganerate array/Program.cs
@@ -6,15 +6,18 @@ namespace Aotoganerate_array
     {
         static void Main(string[] args)
         {
-            int[] array =new int [30];
-            int number=0;
+            int[] array =new int [20];
+            array[0] = 0;
+            //int number=0;
             Console.WriteLine("得到的随机数组为：");
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 1; i < 15; i++)
             {
-                int random = new Random().Next(10);
-                number += random;
-                array[i] = number;
-                Console.Write(" "+array[i]);
+                //int random = new Random().Next(1, 20);
+                //number += new Random().Next(1, 5);
+                array[i] = array[i-1]+new Random().Next(1,5);
+
+                //array[i] = array[i - 1] + new Random().Next(1, 5);
+                Console.Write(" " + array[i-1]);
             }
             Console.ReadLine();
         }
