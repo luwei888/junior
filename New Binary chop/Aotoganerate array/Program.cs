1cb3d6abc1010fd3de04ee15660171df06e56fa7
luwei.jin@outlook.com <luwei.jin@outlook.com>
2019-02-22 15:56:40 +0800

    Aotoganerrate array

---
+++ New Binary chop/Aotoganerate array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aotoganerate_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array =new int [30];
+            int number=0;
+            Console.WriteLine("得到的随机数组为：");
+            for (int i = 0; i < array.Length; i++)
+            {
+                int random = new Random().Next(10);
+                number += random;
+                array[i] = number;
+                Console.Write(" "+array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
