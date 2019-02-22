478fafba5830c6bf320d064a20d5329628dff984
luwei.jin@outlook.com <luwei.jin@outlook.com>
2019-02-22 15:26:01 +0800

    合并项目

---
+++ New-nimigame/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace New_nimigame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int random = new Random().Next(500);
+            int number = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("请输入一个数：");
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number == random)
+                {
+                    Console.WriteLine("恭喜你答对了！");
+                    break;
+                }
+                else if (number > random)
+                {
+                    Console.WriteLine($"太大了，继续猜！只剩{9 - i}次了哦！");
+                }
+                else
+                    Console.WriteLine($"太小了，继续猜！只剩{9 - i}次了哦！");
+            }
+            if(number!=random)
+            {
+                Console.WriteLine($"你太菜啦！这都不会，正确答案是{random}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
