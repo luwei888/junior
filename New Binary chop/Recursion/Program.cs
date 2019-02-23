a4255075555f10867fe98416fe7dda304ad9c728
luwei.jin@outlook.com <luwei.jin@outlook.com>
2019-02-23 14:09:38 +0800

    递归学习

---
+++ New Binary chop/Recursion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("输入N的值：");
+            
+        }
+
+        public int F(int n)
+        {
+            int result;
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+                result = F(n - 1) * n;
+            return result;
+        }
+    }
+}
