f3c8eb1e64112557f3c1b56260a0af5d383ad4ae
luwei.jin@outlook.com <luwei.jin@outlook.com>
2019-02-24 11:50:19 +0800

    随机数组生成函数

--- New Binary chop/Aotoganerate array/Program.cs
+++ New Binary chop/Aotoganerate array/Program.cs
@@ -6,20 +6,32 @@ namespace Aotoganerate_array
     {
         static void Main(string[] args)
         {
-            int[] array =new int [20];
-            array[0] = 0;
-            //int number=0;
-            Console.WriteLine("得到的随机数组为：");
-            for (int i = 1; i < 15; i++)
+            int variable = 5;
+            int[] array2 = Array(variable);
+            Console.WriteLine("输出数组：");
+            for (int i = 0; i < variable; i++)
             {
-                //int random = new Random().Next(1, 20);
-                //number += new Random().Next(1, 5);
-                array[i] = array[i-1]+new Random().Next(1,5);
-
-                //array[i] = array[i - 1] + new Random().Next(1, 5);
-                Console.Write(" " + array[i-1]);
+                Console.Write(array2[i]+" ");
             }
             Console.ReadLine();
         }
+        static int[] Array(int A)
+        {
+            int[] array = new int[A];
+            array[0] = 0;
+            //int number=0;
+            Console.WriteLine("得到的随机数组为：");
+            for (int i = 1; i < A; i++)
+            {
+                //int random = new Random().Next(1, 20);
+                //number += new Random().Next(1, 5);
+                array[i] = array[i - 1] + new Random().Next(1, 5);
+               
+                //array[i] = array[i - 1] + new Random().Next(1, 5);
+            }
+            return array;
+        }
+       
+        
     }
 }
---
+++ New Binary chop/数据类型/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace 数据类型
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("A/B="+divide(10,2));
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+        static float divide (float A,float B)
+        {
+            float number= A / B;
+            return number ;
+        }
+    }
+}
