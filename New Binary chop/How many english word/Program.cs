181934f18f65658dc2778db2f95306fb03d94668
luwei.jin@outlook.com <luwei.jin@outlook.com>
2019-04-09 15:07:16 +0800

    SQL键_约束_索引区别

--- New Binary chop/How many english word/Program.cs
+++ New Binary chop/How many english word/Program.cs
@@ -6,7 +6,12 @@ namespace How_many_english_word
     {
         static void Main(string[] args)
         {
-            string demo = "Yuanzhan, one of the most famous training course，who aim to provide the best service for those who want to change their life by learning computer program skill was created by Mr.YeFei, a who   first was a lawyer after graduted  from the Southwest University of Political Science and Law with the Economic and Law bachelor degrees in 20 02 but   at last came into the IT field as a software developer in 200 7.... ..";
+            string demo = "Yuanzhan, one of the most famous training course，" +
+                "who aim to provide the best service for those who want to change their" +
+                " life by learning computer program skill was created by Mr.YeFei, a who  " +
+                " first was a lawyer after graduted  from the Southwest University of Political " +
+                "Science and Law with the Economic and Law bachelor degrees in 20 02 but   " +
+                "at last came into the IT field as a software developer in 200 7.... ..";
             int number = 0;
             int i = 0;
             int temp = 0;
---
+++ SQLServer/SQLQuery键_约束_索引区别.sql
@@ -0,0 +1,54 @@
+﻿-- 作业：
+
+--新建表TMessage(Id, FromUser, ToUser, UrgentLevel, 
+--Kind, HasRead, IsDelete, Content)，使用该表：
+
+--    证明“唯一约束依赖于唯一索引”
+--    证明“主键上可以是非聚集索引”
+--    证明“SQL Server不会为外键自动添加索引
+
+
+--CREATE TABLE TMessage 
+--(
+--  [Id] INT ,
+--  [FromUser] INT ,
+--  [ToUser] INT,
+--  [UrgentLevel] INT,
+--  [Kind] INT,
+--  [HasRead] BIT,
+--  [IsDelete] BIT,
+--  [Content] INT,
+--)
+
+--    证明“唯一约束依赖于唯一索引”
+ALTER TABLE TMessage
+ADD  CONSTRAINT  UCON_Kind  UNIQUE(Kind)
+
+SELECT [name], [type],  is_unique, is_primary_key, is_unique_constraint
+FROM sys.indexes 
+WHERE [name] = 'UCON_Kind'
+
+
+ --证明“主键上可以是非聚集索引”
+
+ ALTER TABLE TMessage 
+ALTER COLUMN Id INT Not Null
+
+CREATE   CLUSTERED	INDEX  [Clu_HasRead]		ON TMessage(HasRead)
+
+ALTER TABLE TMessage
+ADD CONSTRAINT PK_Message_Id PRIMARY KEY  (Id)
+
+SELECT [name], [type],  is_unique, is_primary_key, is_unique_constraint
+FROM sys.indexes 
+WHERE [name] = 'PK_Message_Id'
+
+
+--    证明“SQL Server不会为外键自动添加索引
+
+ALTER TABLE TMessage
+ADD CONSTRAINT FK_Message_FromUser FOREIGN KEY  (FromUser)  REFERENCES  TMessage(Id)
+
+SELECT [name], [type],  is_unique, is_primary_key, is_unique_constraint
+FROM sys.indexes 
+WHERE [name] = 'FK_Message_FromUser'
