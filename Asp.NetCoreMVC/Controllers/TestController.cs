360a6450df1a533edd148333b4725d5198395d1b
luwei.jin@outlook.com <luwei.jin@outlook.com>
2019-04-22 16:30:33 +0800

    JavaScript及.netmvc学习

---
+++ Asp.NetCoreMVC/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreMVC.Controllers
+{
+    //引入Controller基类，包含常用的对象方法，方便获取数据，输出信息
+    public class TestController : Controller
+    {
+        public IActionResult SayHello()
+        {
+            //查找索引方式，key的名称
+            string value = Request.Query["name"];
+            //Request.Form["key"] 获取表单
+            //Request.Cookies[""] 获取cookies
+            //Request.Headers[""] 获取 header
+
+            //获取，移除，设置
+            HttpContext.Session.SetString("name", "feige");
+            HttpContext.Session.SetInt32("age", 24);
+            HttpContext.Session.GetString("name");
+            HttpContext.Session.Remove("name");
+            int? age = HttpContext.Session.GetInt32("age");
+            return Content("hello" + value);
+        }
+    }
+}
--- Java Script Learn/wwwroot/js/JavaScriptLearn2.js
+++ Java Script Learn/wwwroot/js/JavaScriptLearn2.js
@@ -50,3 +50,12 @@ function isWord(word) {
     }
     return false;
 }
+
+
+var two = 0.2;
+var one = 0.1;
+var eight = 0.8;
+var six = 0.6;
+
+two - one == one;   //true
+eight - six == two;  //false
