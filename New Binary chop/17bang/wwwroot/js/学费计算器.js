bbfbd3675ff4043c2e06eabcefbdaad7ddd6ef56
luwei.jin@outlook.com <luwei.jin@outlook.com>
2019-05-06 21:15:58 +0800

    Js学费计算器完善

---
+++ New Binary chop/17bang/wwwroot/js/学费计算器.js
@@ -0,0 +1,10 @@
+﻿
+
+var oBtn = document.getElementById("btn");
+oBtn.onclick = function () {
+    var startTime = Date.parse(document.getElementById("startTime").value);
+    var endTime = Date.parse(document.getElementById("endTime").value);
+    var studyWeek = (endTime - startTime) / 1000 / 3600 / 24 / 7;
+    var money = studyWeek * 986;
+    alert(`学习${studyWeek}周，学费为${money}`);
+};
