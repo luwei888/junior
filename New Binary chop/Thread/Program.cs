6410b95e25b44261243206c20d52f72bc4e25bac
luwei <2073414181@qq.com>
2019-06-26 22:36:43 +0800

    .Net Core 发布

--- New Binary chop/Thread/Program.cs
+++ New Binary chop/Thread/Program.cs
@@ -6,21 +6,11 @@ namespace Threadplace
 {
     class Program
     {
-        public void Test()
-        {
-            ThreadStart threadStart = new ThreadStart(Calculate);
-            Thread thread = new Thread(threadStart);
-            thread.Start();
-        }
-
-        public void Calculate()
-        {
-            double Diameter = 0.5;
-            Console.Write("The perimeter Of Circle with a Diameter of {0} is {1}", Diameter, Diameter * Math.PI);
-        }
 
         static void Main(string[] args)
         {
+            Console.WriteLine(Thread.CurrentThread);
+            var current = Thread.CurrentThread;
             Console.ReadLine();
         }
     }
